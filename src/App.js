00309bd0837e1725c1a8535f6c738484f4ec7920
danny <daniel_hitchen@outlook.com>
2021-02-23 18:20:57 +0000

    react calculator app

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+    state = {
+        result: [],
+        keys: [
+       {value:"clear", styleName:"clearButton"},
+       {value:"/", styleName:"operatorButton"},
+       {value:"7", styleName:"numberButton"},
+       {value:"8", styleName:"numberButton"},
+       {value:"9", styleName:"numberButton"},
+       {value:"*", styleName:"operatorButton"},
+       {value:"4", styleName:"numberButton"},
+       {value:"5", styleName:"numberButton"},
+       {value:"6", styleName:"numberButton"},
+       {value:"-", styleName:"operatorButton"},
+       {value:"1", styleName:"numberButton"},
+       {value:"2", styleName:"numberButton"},
+       {value:"3", styleName:"numberButton"},
+       {value:"+", styleName:"operatorButton"},
+       {value:"0", styleName:"zeroButton"},
+       {value:".", styleName:"numberButton"},
+       {value:"=", styleName:"operatorButton"},
+     ]
+    };
+
+
+    handleButton = (value) => {
+        if (value === "clear") {
+            this.setState({ result: [] });
+        } else if (value === "=") {
+            this.setState({ result: [eval(this.state.result.join(""))] });
+        } else {
+            this.setState({ result: [...this.state.result, value] });
+        }
+
+    };
+    
+
+
+                    render() {
+                        const { result, keys } = this.state
+
+                        return (
+                            <div className="container1">
+                                <div className="container2">
+                                    <h1 className="display"> {result.length === 0 ? "0" : result}</h1>
+                                    <div className="keyboard-container">
+                                        {keys.map((key) => {
+                                            return (
+                                                <button className={key.styleName}
+                                                    onClick={() => this.handleButton(key.value)}>
+                                                    {key.value}
+                                                </button>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+
+                    )
+                }
+
+
+
+
+}
+
+  
+
+
+
+
+export default App;
